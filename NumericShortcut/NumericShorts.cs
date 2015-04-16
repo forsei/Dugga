d992f13483a06d550eb7cc30317f3eacd7073cfa
Sing Pang <sing_pang@hotmail.com>
2015-04-16 11:12:03 +0200

    code som inte funkar

--- NumericShortcut/NumericShorts.cs
+++ NumericShortcut/NumericShorts.cs
@@ -15,14 +15,65 @@ namespace NumericShortcut
                 throw new ArgumentNullException("Message is null");
             }
 
-            if (shortcut.Trim().Equals(""))
+            if (shortcut.Equals(""))
             {
                 throw new ArgumentException("Message is empty.");
             }
 
 
+            string[] thousand = { "k" };
 
-            return "1000";
+
+
+            return shortcut;
+
+
+
+            //if (shortcut > 1000)
+            //{
+            //    return string.Concat(shortcut / 1000, "k", "K");
+            //}
+            //else
+            //{
+            //    return shortcut.ToString();
+            //}
+
+
+
+
+            //if (shortcut > 1000000)
+            //{
+            //    return string.Concat(shortcut / 1000000, "m", "M");
+            //}
+            //else
+            //{
+            //    return shortcut.ToString();
+            //}
+
+
+
+
+            //if (shortcut > 1000000000)
+            //{
+            //    return string.Concat(shortcut / 1000000000, "b", "B");
+            //}
+            //else
+            //{
+            //    return shortcut.ToString();
+            //}
+
+
+            //if (shortcut > 1000000000000)
+            //{
+            //    return string.Concat(shortcut / 1000000000000, "t", "T");
+            //}
+            //else
+            //{
+            //    return shortcut.ToString();
+            //}
+
+
+            
         }
     }
 }
